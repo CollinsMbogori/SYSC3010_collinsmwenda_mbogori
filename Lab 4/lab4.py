8323d76c2ce1b656b4c51fd83fb40fc416884dc3
CollinsMbogori <71289466+CollinsMbogori@users.noreply.github.com>
2020-10-05 17:01:59 -0400

    [collinsmwendambogori] feat: Changes to a program that writes data in a ThinkSpeak channel
    The program writes data in fields in the given think speak channel.

---
+++ Lab 4/lab4.py
@@ -0,0 +1,14 @@
+import http.client
+import urllib.request
+
+def write_data():
+    key = "54CDA6FJF4RCJ92C"
+    url = 'https://api.thinkspeak.com/update?api_key='
+    header = '&field1={} &field2{} &field3{}'.format(collinsmwendambogori@cmail.carleton.ca,L2-M-9,A)
+    new_url = url+key+header
+    temp1 = urlib.request.urlopen(new_url)
+    print(temp1)
+
+#if __name__ == '__main__':
+    
+#     write_data()
